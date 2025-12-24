88338a3d36f5275e596dabad4dc6c492a6c24d98
manueljose199-spec <manueljoseemma199@gmail.com>
2025-12-24 13:46:09 -0600

    feat: se creo funcion en el scrip para un footer cookies interactivo

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ botonTema.addEventListener('click', ()=>{
 
     iconoTema.textContent=body.classList.contains('light')?'☀️' : '🌙';
 });
+
+
+
+const bannerCookies=document.getElementById('bannerCookies');
+const aceptarCookies=document.getElementById('ceptarCookies');
+
+if (!localStorage.getItem('cookiesAceptadas')){
+    setTimeout(()=>{
+        bannerCookies.classList.add('mostrar')
+    },10000
+    )
+}
+
+aceptarCookies.addEventListener('click', ()=>{
+    localStorage.setItem('cookiesAceptadas', 'true');
+    bannerCookies.classList.remove('mostrar')
+})
